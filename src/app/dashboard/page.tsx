524c91575c76d7d143496b28d06b87f1dd53d1b7
Lori Civello <lacivello@gmail.com>
2025-07-04 21:49:21 -0400

    Add dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function DashboardPage() {
+  return (
+    <div className="container mx-auto p-8">
+      <h1 className="text-4xl font-bold">My Dashboard</h1>
+      <p className="mt-4">This is where your pending appointments will be displayed.</p>
+    </div>
+  );
+}
