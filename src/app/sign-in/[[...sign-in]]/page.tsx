a073b5b4d210db9aca7b2b81d5b97f07eb4a01d4
lori <lacivello@gmail.com>
2025-07-28 22:15:05 -0500

    Update Clerk sign-in and sign-up pages

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,11 @@
 // src/app/sign-in/[[...sign-in]]/page.tsx
 import { SignIn } from "@clerk/nextjs";
+import React from "react";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <SignIn routing="path" path="/sign-in" />
+    </div>
+  );
 }
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,6 +1,11 @@
 // src/app/sign-up/[[...sign-up]]/page.tsx
 import { SignUp } from "@clerk/nextjs";
+import React from "react";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <SignUp routing="path" path="/sign-up" />
+    </div>
+  );
 }
